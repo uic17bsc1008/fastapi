c83b7e237cf3f2f38195b20a4d1f712c36f9197f
Rajiv Nagar <uic.17bsc1008@gmail.com>
2024-10-11 16:39:26 +0530

    user router get and delete function add

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from models.users import User
 from engine import SessionDep
 from sqlmodel import select
@@ -16,3 +16,19 @@ async def create_user(user: User, session: SessionDep):
 @router.get('/users/')
 async def all_users(session: SessionDep):
     return session.exec(select(User)).all()
+
+@router.get('/user/details/{user_id}/')
+async def get_user(user_id : int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+@router.get('/user/delete/{user_id}/')
+def delete_hero(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}
