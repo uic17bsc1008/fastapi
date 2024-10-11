88947c05cb3cb02df6b09b4ffbac0173f9b90bfc
Rajiv Nagar <uic.17bsc1008@gmail.com>
2024-10-11 16:42:57 +0530

    cors middleware added

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from routers import users
 import uvicorn
 from sqlmodel import SQLModel
 from engine import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Database Config
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+# Database Config End
 
 # App Config
-
 app = FastAPI()
 app.include_router(users.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# App Config End
 
 @app.on_event("startup")
 def on_startup():
